80579d70fe549c1df3810fc19f7e017c0c832c55
Xfly <18374858141@163.com>
2023-07-07 10:56:26 +0800

    Update find_position_in_gff.py
    bug fixed

--- find_position_in_gff.py
+++ find_position_in_gff.py
@@ -1,7 +1,21 @@
 import argparse
-import copy
+"""
+Data Structure:
+The chromosome name is the key, and all mRNAs under a single chromosome are the values and stored in a list, 
+each individual mRNA is nested in the list, each individual mRNA is taken as the first value by the mRNA line in the gff file,
+and its cds are sorted sequentially.
+
+Exameple:
+{'NC_000001.11': 
+    [[[17300, 17700], [17369, 17436], [17450, 17472], [17510, 17600]], 
+    [[18300, 18700], [18369, 18436], [18450, 18472], [18510, 18600]]], 
+'NC_000001.22': 
+    [[[17300, 17700], [17369, 17436], [17450, 17472], [17510, 17600]],
+    [[18300, 18700], [18369, 18436], [18450, 18472], [18510, 18600]]]}
+"""
 def get_mRNA_list(gff_file):
     mRNA_list = {}
+    sort_mRNA_list={}
     with open(gff_file, 'r') as f:
         for line in f:
             if not line.startswith('#'):
@@ -15,18 +29,24 @@ def get_mRNA_list(gff_file):
                 if feature == "mRNA":
                     mRNA_list[chromosome].append([])
                 mRNA_list[chromosome][-1].extend([[start, end]])
-    return mRNA_list
+    
+    for key,value in mRNA_list.items(): #sort mRNA list to pinpoint the order of exons
+        sort_list=[]
+        for single_mRNA in value:
+            sort_single_mRNA = sorted(single_mRNA,key=lambda x: x[0])
+            sort_list.append(sort_single_mRNA)
+        sort_mRNA_list[key] = sort_list
+    return sort_mRNA_list
+
                 
 def get_snp_location(snp_chromosome,snp_position,mRNA_list):
-    tmp_mRNA_list = copy.deepcopy(mRNA_list)
-    if snp_chromosome in tmp_mRNA_list:
-        specified_chromosome_data=tmp_mRNA_list[snp_chromosome]
+    if snp_chromosome in mRNA_list:
+        specified_chromosome_data=mRNA_list[snp_chromosome]
         for mrna_index,mRNA in enumerate(specified_chromosome_data):
-            mRNA_quantity=len(tmp_mRNA_list)
+            mRNA_quantity=len(specified_chromosome_data)
             if mRNA[0][0]<=snp_position<=mRNA[0][1]:
-                mRNA.pop(0)
-                CDS_quantity=len(mRNA)
-                for cds_index,pos in enumerate(mRNA):
+                CDS_quantity=len(mRNA)-1 # Count the number of CDS
+                for cds_index,pos in enumerate(mRNA[1:]):
                     if pos[0]<=snp_position<=pos[1]:
                         print(f'{snp_chromosome}\t{snp_position}\texon\t({cds_index+1})')
                         break
@@ -66,6 +86,8 @@ def main():
 if __name__ == "__main__":
     main()
     # example： python find_position_in_gff.py -p pos.txt -g example.gff
-    # output: 17510   exon    (3)
-    #         17480   intron
-    #         111111  intergenic
+    # output: NC_000001.11    17510   exon    (3)
+    #         NC_000001.11    17480   intron
+    #         NC_000001.22    17510   exon    (3)
+    #         NC_000001.22    17480   intron
+    #         NC_000001.22    11111111        intergenic
